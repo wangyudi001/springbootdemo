6219694eedd016d07734299e64759aea8a173109
wangyudi001 <473004341@qq.com>
2019-06-18 11:46:18 +0800

    添加返回自增id

--- src/main/java/com/boot/web/UserController.java
+++ src/main/java/com/boot/web/UserController.java
@@ -27,30 +27,31 @@ public class UserController {
     @UserLoginToken
     public RenderJson delUserById(@RequestParam("id") Integer id) {
         Boolean bool = userService.delUserById(id);
-        return  bool? RenderJson.Ok("0", "Success") : RenderJson.No("1", "Fail", "Unknown Error!");
+        return bool ? RenderJson.Ok("0", "Success") : RenderJson.No("1", "Fail", "Unknown Error!");
     }
 
     @RequestMapping(value = "/upd/{id}", method = RequestMethod.POST)
     @UserLoginToken
-    public RenderJson updUserById(@PathVariable String id, @RequestBody User user){
+    public RenderJson updUserById(@PathVariable String id, @RequestBody User user) {
         user.setUserId(id);
         Boolean bool = userService.updUserById(user);
-        return bool? RenderJson.Ok("0", "Success") : RenderJson.No("1", "Fail", "Unknown Error!");
+        return bool ? RenderJson.Ok("0", "Success") : RenderJson.No("1", "Fail", "Unknown Error!");
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     @UserLoginToken
-    public RenderJson addUser(@RequestBody User user){
+    public RenderJson addUser(@RequestBody User user) {
         int count = userService.getCountByName(user.getUserName());
-        if (count >=  1){
+        if (count >= 1) {
             return RenderJson.No("1", "Fail", "This user already exists!");
         }
         Boolean bool = userService.addUser(user);
-        return bool? RenderJson.Ok("0", "Success") : RenderJson.No("1", "Fail", "Unknown Error");
+        String i = user.getUserId(); // 获取自增结果的id
+        return bool ? RenderJson.Ok("0", "Success", i) : RenderJson.No("1", "Fail", "Unknown Error");
     }
 
     @RequestMapping(value = "/exist", method = RequestMethod.GET)
-    public RenderJson getUserCount(@RequestParam("username") String username){
+    public RenderJson getUserCount(@RequestParam("username") String username) {
         int count = userService.getCountByName(username);
         return RenderJson.Ok("0", "Success", count);
     }
